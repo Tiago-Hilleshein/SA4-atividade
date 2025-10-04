b9b8ec5a7c45fc3e3d3278ff938ceddeee8d9e52
Tiago-Hilleshein <tiagohilleshein@gmail.com>
2025-10-04 13:54:44 -0300

    Delete sa4-teste-main directory

--- sa4-teste-main/page_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from page_app.views import index, contato
-
-urlpatterns = [
-    # Cadastrar URLs aqui
-    path('', index),
-
-    path('contato/', contato),
-]
--- sa4-teste-main/page_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index (request):
-    return render(request,"page_app/partial/home.html")
-
-def contato (request):
-    return render(request, "page_app/partial/contato.html")
